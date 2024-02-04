4680c6eccea3c5ff12f4b8e826da66c196c65859
Khushi Agarwal <magickhu@gmail.com>
2024-02-04 09:44:45 +0530

    Add files via upload

---
+++ basics/index.js
@@ -0,0 +1,9 @@
+//console.log(`hello`);
+//window.alert(`this is the alert`);
+
+//this is a comment
+/* 
+this is a multiline comment */
+
+document.getElementById("myH1").textContent = `hello`;
+document.getElementById("myP").textContent=`th is a para`;
---
+++ variable/index.js
@@ -0,0 +1,15 @@
+let age= 15;
+let price = 10.99;
+let gpa = 2.1;
+let fullname= "khushi";
+
+console.log(typeof name);
+console.log(`your are is ${age} years old`);
+console.log(`you gpa is ${gpa}`);
+
+//booleans
+let online =true;
+
+document.getElementById('p1').textContent = `Your fullname is ${fullname}`;
+document.getElementById('p2').textContent = `Your age is ${age} years old`;
+document.getElementById('p3').textContent = `Your gpa is ${gpa}`;
